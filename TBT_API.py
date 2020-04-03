8bec33dd38821a9c33d36b3545636ee06e0fcf98
ztjryg4 <ztjryg4@163.com>
2020-04-04 00:07:11 +0800

    upload api.ver2

--- TBT_API.py
+++ TBT_API.py
@@ -22,20 +22,24 @@ def routeResponse():
     data = request.get_json()
     mode = data['mode']
     if mode == "both":
+        # case1: 
+        # distance_matrix, demands, dep_starts, dep_ends, vehicle_capacities
+        distance_matrix = data['distance_matrix']
+        demands = data['demands']
         dep_starts = data['dep_starts']
         dep_ends = data['dep_ends']
         vehicle_capacities = data['vehicle_capacities']
-        if not all([dep_starts, dep_ends, vehicle_capacities]):
+        if not all([distance_matrix, demands, dep_starts, dep_ends, vehicle_capacities]):
             js = TBT_func.ErrorGen("TBT Param Error: Missing Param.")
             res = Response(js, status=200, mimetype='application/json')
             return res
             # TODO: Exception Handling
         else:
-            # js = TBT_func.case1(dep_starts, dep_ends, vehicle_capacities)
+            # js = TBT_func.case1(distance_matrix, demands, dep_starts, dep_ends, vehicle_capacities)
             # res = Response(js, status=200, mimetype='application/json')
             # return res
             try:
-                js = TBT_func.case1(dep_starts, dep_ends, vehicle_capacities)
+                js = TBT_func.case1(distance_matrix, demands, dep_starts, dep_ends, vehicle_capacities)
             except:
                 js = TBT_func.ErrorGen("TBT Unknow Error.")
                 res = Response(js, status=200, mimetype='application/json')
@@ -44,20 +48,23 @@ def routeResponse():
                 res = Response(js, status=200, mimetype='application/json')
                 return res
     elif mode == "num":
+        # distance_matrix, demands, dep_starts, dep_ends, num_vehicles
+        distance_matrix = data['distance_matrix']
+        demands = data['demands']
         dep_starts = data['dep_starts']
         dep_ends = data['dep_ends']
         num_vehicles = data['num_vehicles']
-        if not all([dep_starts, dep_ends, num_vehicles]):
+        if not all([distance_matrix, demands, dep_starts, dep_ends, num_vehicles]):
             js = TBT_func.ErrorGen("TBT Param Error: Missing Param.")
             res = Response(js, status=200, mimetype='application/json')
             return res
             # TODO: Exception Handling
         else:
-            # js = TBT_func.case2(dep_starts, dep_ends, num_vehicles)
+            # js = TBT_func.case2(distance_matrix, demands, dep_starts, dep_ends, num_vehicles)
             # res = Response(js, status=200, mimetype='application/json')
             # return res
             try:
-                js = TBT_func.case2(dep_starts, dep_ends, num_vehicles)
+                js = TBT_func.case2(distance_matrix, demands, dep_starts, dep_ends, num_vehicles)
             except:
                 js = TBT_func.ErrorGen("TBT Unknow Error.")
                 res = Response(js, status=200, mimetype='application/json')
@@ -66,16 +73,51 @@ def routeResponse():
                 res = Response(js, status=200, mimetype='application/json')
                 return res
     elif mode == "cap":
-        depot = data['depot']
+        # distance_matrix, demands, depot, vehicle_capacities
+        distance_matrix = data['distance_matrix']
+        demands = data['demands']
+        depot = data['dep_starts'][0]
         vehicle_capacities = data['vehicle_capacities']
-        if (not all([depot, vehicle_capacities])) and depot != 0:
+        if (not all([distance_matrix, demands, depot, vehicle_capacities])) and depot != 0:
             js = TBT_func.ErrorGen("TBT Param Error: Missing Param.")
             res = Response(js, status=200, mimetype='application/json')
             return res
             # TODO: Exception Handling
         else:
+            # js = TBT_func.case3(distance_matrix, demands, depot, vehicle_capacities)
+            # res = Response(js, status=200, mimetype='application/json')
+            # return res
             try:
-                js = TBT_func.case3(depot, vehicle_capacities)
+                js = TBT_func.case3(distance_matrix, demands, depot, vehicle_capacities)
+            except:
+                js = TBT_func.ErrorGen("TBT Unknow Error.")
+                res = Response(js, status=200, mimetype='application/json')
+                return res
+            else:
+                res = Response(js, status=200, mimetype='application/json')
+                return res
+    elif mode == "takeaway":
+        # distance_matrix, dep_starts, pickups_deliveries, pick_weights='', weight_limits=''
+        # case4(distance_matrix, dep_starts, pickups_deliveries, pick_weights='', weight_limits='')
+        distance_matrix = data['distance_matrix']
+        dep_starts = data['dep_starts']
+        pickups_deliveries = data['pickups_deliveries']
+        pick_weights = data['pick_weights']
+        weight_limits = data['weight_limits']
+
+        print(TBT_func.case4(distance_matrix, dep_starts, pickups_deliveries, pick_weights, weight_limits))
+
+        if (not all([distance_matrix, dep_starts, pickups_deliveries, pick_weights, weight_limits])):
+            js = TBT_func.ErrorGen("TBT Param Error: Missing Param.")
+            res = Response(js, status=200, mimetype='application/json')
+            return res
+            # TODO: Exception Handling
+        else:
+            # js = TBT_func.case4(distance_matrix, dep_starts, pickups_deliveries, pick_weights, weight_limits)
+            # res = Response(js, status=200, mimetype='application/json')
+            # return res
+            try:
+                js = TBT_func.case4(distance_matrix, dep_starts, pickups_deliveries, pick_weights, weight_limits)
             except:
                 js = TBT_func.ErrorGen("TBT Unknow Error.")
                 res = Response(js, status=200, mimetype='application/json')
--- TBT_func.py
+++ TBT_func.py
@@ -19,15 +19,15 @@ def ErrorGen(ErrorType):
     js = json.dumps(msg, ensure_ascii=False)
     return js
 
-def case1(dep_starts, dep_ends, vehicle_capacities):
+def case1(distance_matrix, demands, dep_starts, dep_ends, vehicle_capacities):
     '''
     有车子数量以及每辆车的限载的约束时，输入参数为 dep_starts, dep_ends, num_vehicles, vehicle_capacities。
     '''
-    b_loc = pd.read_csv(r'./BasicLocations.csv')
-    b_dist = squareform(pdist(b_loc.values[:, 1:3])).astype(int)
-    
+    # b_loc = pd.read_csv(r'./BasicLocations.csv')
+    # b_dist = squareform(pdist(b_loc.values[:, 1:3])).astype(int)
+    b_dist = np.array(distance_matrix)
     vrp = VRP(
-        distance_matrix=b_dist, demands=b_loc['demand'], dep_starts=dep_starts, dep_ends=dep_ends,
+        distance_matrix=b_dist, demands=demands, dep_starts=dep_starts, dep_ends=dep_ends,
         num_vehicles=len(vehicle_capacities),
         vehicle_capacities=vehicle_capacities,
         travel_time_limit=int(1e9),
@@ -48,15 +48,15 @@ def case1(dep_starts, dep_ends, vehicle_capacities):
     js = json.dumps(msg, ensure_ascii=False)
     return js
 
-def case2(dep_starts, dep_ends, num_vehicles):
+def case2(distance_matrix, demands, dep_starts, dep_ends, num_vehicles):
     '''
     只给定车子数量，输入参数为 dep_starts, dep_ends, num_vehicles
     '''
-    b_loc = pd.read_csv(r'./BasicLocations.csv')
-    b_dist = squareform(pdist(b_loc.values[:, 1:3])).astype(int)
-
+    # b_loc = pd.read_csv(r'./BasicLocations.csv')
+    # b_dist = squareform(pdist(b_loc.values[:, 1:3])).astype(int)
+    b_dist = np.array(distance_matrix)
     vrp = VRP(
-        distance_matrix=b_dist, demands=b_loc['demand'], dep_starts=dep_starts, dep_ends=dep_ends,
+        distance_matrix=b_dist, demands=demands, dep_starts=dep_starts, dep_ends=dep_ends,
         num_vehicles=num_vehicles,
         # vehicle_capacities=b_capa,
         travel_time_limit=int(1e9),
@@ -78,16 +78,15 @@ def case2(dep_starts, dep_ends, num_vehicles):
     js = json.dumps(msg, ensure_ascii=False)
     return js
 
-def case3(depot, vehicle_capacities):
+def case3(distance_matrix, demands, depot, vehicle_capacities):
     '''
     只给定限载的约束，这时，限制为一个配送中心，输入参数为 depot, vehicle_capacities，其中vehicle_capacities 内元素unique
     '''
-    b_loc = pd.read_csv(r'./BasicLocations.csv')
-    b_dist = squareform(pdist(b_loc.values[:, 1:3])).astype(int)
-
-    b_capa = [14, 16]
+    # b_loc = pd.read_csv(r'./BasicLocations.csv')
+    # b_dist = squareform(pdist(b_loc.values[:, 1:3])).astype(int)
+    b_dist = np.array(distance_matrix)
     vrp = VRP(
-        distance_matrix=b_dist, demands=b_loc['demand'], depot=depot,
+        distance_matrix=b_dist, demands=demands, depot=depot,
         # num_vehicles=len(b_capa),
         vehicle_capacities=vehicle_capacities,
         travel_time_limit=int(1e9),
@@ -109,16 +108,83 @@ def case3(depot, vehicle_capacities):
     js = json.dumps(msg, ensure_ascii=False)
     return js
 
+def case4(distance_matrix, dep_starts, pickups_deliveries, pick_weights=None, weight_limits=None):
+    distance_matrix = np.array(distance_matrix)
+    if (pick_weights == None) or (weight_limits == None):
+        print("No pick_weights or weight_limits")
+        vrp = VRP(
+            distance_matrix=distance_matrix, dep_starts=dep_starts, pickups_deliveries=pickups_deliveries,
+            num_vehicles=len(dep_starts),
+            max_travel_distance=3000
+        )
+    else:
+        print("exist pick_weights or weight_limits")
+        vrp = VRP(
+            distance_matrix=distance_matrix, dep_starts=dep_starts, pickups_deliveries=pickups_deliveries,
+            num_vehicles=len(dep_starts),
+            max_travel_distance=3000,
+            pick_weights=pick_weights,
+            weight_limits=weight_limits
+        )
+    vrp.solve()
+    result = vrp.result_pd()
+    # print(result['routes'])
+    print(result)
+    msg = {
+        "status": "success",
+        "data": {
+            "routes": result['routes'],
+            "distance_of_routes": np.array(result['distance_of_routes']).tolist(),
+            "num_vehicles": result['num_vehicles'],
+            # "vehicle_capacities": np.array(vehicle_capacities).tolist(),
+        }
+    }
+    js = json.dumps(msg, ensure_ascii=False)
+    return js
+
 if __name__ == "__main__":
 
     vehicle_capacities = [14, 14, 16, 16]
     dep_starts = [0, 0, 1, 1]
     dep_ends = dep_starts
     num_vehicles = 4
+    distance_matrix = [
+        [   0,  392,  557,  515,  417,  196,  377,  139,  241,  139,  462, 377,  278,  265,  331,  557,  468 ],
+        [ 392,    0,  684,  241,  139,  377,  592,  265,  515,  526,  792, 493,  480,  571,  722,  679,  857 ],
+        [ 557,  684,    0,  915,  802,  377,  196,  618,  331,  526,  400, 931,  835,  799,  604, 1114,  650 ],
+        [ 515,  241,  915,    0,  114,  575,  802,  377,  702,  653,  966, 415,  460,  589,  835,  560,  974 ],
+        [ 417,  139,  802,  114,    0,  462,  688,  278,  592,  557,  859, 400,  415,  531,  745,  571,  884 ],
+        [ 196,  377,  377,  575,  462,    0,  228,  241,  139,  240,  417, 557,  468,  460,  415,  745,  531 ],
+        [ 377,  592,  196,  802,  688,  228,    0,  462,  139,  331,  265, 755,  653,  606,  415,  931,  480 ],
+        [ 139,  265,  618,  377,  278,  241,  462,    0,  342,  278,  592, 331,  265,  320,  468,  526,  606 ],
+        [ 241,  515,  331,  702,  592,  139,  139,  342,    0,  196,  278, 618,  515,  468,  320,  792,  415 ],
+        [ 139,  526,  526,  653,  557,  240,  331,  278,  196,    0,  342, 462,  351,  278,  196,  618,  331 ],
+        [ 462,  792,  400,  966,  859,  417,  265,  592,  278,  342,    0, 802,  688,  592,  278,  943,  265 ],
+        [ 377,  493,  931,  415,  400,  557,  755,  331,  618,  462,  802,   0,  114,  241,  575,  196,  702 ],
+        [ 278,  480,  835,  460,  415,  468,  653,  265,  515,  351,  688, 114,    0,  139,  462,  278,  592 ],
+        [ 265,  571,  799,  589,  531,  460,  606,  320,  468,  278,  592, 241,  139,    0,  342,  351,  462 ],
+        [ 331,  722,  604,  835,  745,  415,  415,  468,  320,  196,  278, 575,  462,  342,    0,  688,  139 ],
+        [ 557,  679, 1114,  560,  571,  745,  931,  526,  792,  618,  943, 196,  278,  351,  688,    0,  798 ],
+        [ 468,  857,  650,  974,  884,  531,  480,  606,  415,  331,  265, 702,  592,  462,  139,  798,    0 ]
+    ]
+    demands = [0, 0, 1, 2, 4, 2, 4, 8, 8, 1, 2, 1, 2, 4, 4, 8, 8]
+
+    # print(case1(distance_matrix, demands, dep_starts, dep_ends, vehicle_capacities))
+    # print(case2(dep_starts, dep_ends, num_vehicles))
     
-    # print(case1(dep_starts, dep_ends, vehicle_capacities))
-    print(case2(dep_starts, dep_ends, num_vehicles))
-    
-    vehicle_capacities = [14, 16]
-    depot = 0
+    # vehicle_capacities = [14, 16]
+    # depot = 0
     # print(case3(depot, vehicle_capacities))
+    dep_starts = [1, 6]
+    pickups_deliveries = [
+        [2, 10],
+        [4, 3],
+        [5, 9],
+        [7, 8],
+        [15, 11],
+        [13, 12],
+        [16, 14],
+    ]
+    pick_weights=1
+    weight_limits=[3,3]
+    case4(distance_matrix, dep_starts, pickups_deliveries, pick_weights, weight_limits)
